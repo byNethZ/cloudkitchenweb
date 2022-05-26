70511d8c56e0900aee461cede3052ba75e3c3a24
NethZ <bynethz@gmail.com>
2022-05-25 23:35:50 -0500

    Adding Menu component, service for foodData, Finishing Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderDetailsService } from 'src/app/services/order-details.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  foodData: any;
+  constructor(
+    private _orderService: OrderDetailsService
+  ) { }
 
   ngOnInit(): void {
+    this.foodData = this._orderService.foodDetails;
   }
 
 }
